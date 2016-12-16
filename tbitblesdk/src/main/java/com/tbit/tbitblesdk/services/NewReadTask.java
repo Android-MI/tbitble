b3556b5a4b7bda1d1fd4bea83352f10646c6f7d6
billy96322 <billy96322@gmail.com>
2016-12-16 11:34:36 +0800

    1. 简化ReadTask逻辑 2. 修改ReadTask的BUG

--- tbitblesdk/src/main/java/com/tbit/tbitblesdk/services/NewReadTask.java
+++ tbitblesdk/src/main/java/com/tbit/tbitblesdk/services/NewReadTask.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 public class NewReadTask extends AsyncTask<Void, byte[], Void> {
     private static final String TAG = "NewReadTask";
+    private static final Byte HEAD_FLAG = new Byte((byte) 0xAA);
     private Reader reader;
     private List<Byte> readTemp = Collections.synchronizedList(new ArrayList<Byte>());
 
@@ -39,8 +40,7 @@ public class NewReadTask extends AsyncTask<Void, byte[], Void> {
         while (!isCancelled()) {
             if (readTemp.size() != 0) {
                 // 过滤
-                if (readTemp.get(0).byteValue() == 0xAA) {
-                    readTemp.remove(0);
+                if (!readTemp.get(0).equals(HEAD_FLAG)) {
                     continue;
                 }
                 if (readTemp.size() < 8)
@@ -50,11 +50,9 @@ public class NewReadTask extends AsyncTask<Void, byte[], Void> {
                 try {
                     process();
                 } catch (IndexOutOfBoundsException e) {
-                    readTemp.clear();
                     e.printStackTrace();
                 }
             }
-            SystemClock.sleep(100L);
         }
         return null;
     }
@@ -66,11 +64,8 @@ public class NewReadTask extends AsyncTask<Void, byte[], Void> {
     }
 
     private void process() {
-        Log.d(TAG, "process: start process");
-        // 截取头部
-        List<Byte> head = readTemp.subList(0, 8);
         // 数据包长度
-        int dataPacketLen = head.get(5) & 0xFF;
+        int dataPacketLen = readTemp.get(5) & 0xFF;
         // 等待数据包长度足够
         for (int i = 0; i < 30; i++) {
             if (readTemp.size() - 8 >= dataPacketLen) {
@@ -79,34 +74,27 @@ public class NewReadTask extends AsyncTask<Void, byte[], Void> {
             SystemClock.sleep(100L);
         }
         // 数据包长度不足
-        if (readTemp.size() -8 < dataPacketLen) {
-            Log.d(TAG, "process: data package length NOT enough");
-            for (int i = 0; i < dataPacketLen; i++) {
+        if (readTemp.size() - 8 < dataPacketLen) {
+            for (int i = 0; i < 8; i++) {
                 readTemp.remove(0);
             }
             return;
         }
-        Log.d(TAG, "process: data package length enough");
         // 数据包长度足够，取出数据包
         int totalLength = 8 + dataPacketLen;
-        List<Byte> currentDataList = readTemp.subList(0, totalLength);
         byte[] receiveData = new byte[totalLength];
         for (int i = 0; i < totalLength; i++) {
-            receiveData[i] = currentDataList.get(i);
+            receiveData[i] = readTemp.remove(0);
         }
         // 发布数据
         publishProgress(receiveData);
-        // 清除已经发布的数据
-        for (int i = 0; i < totalLength; i++) {
-            readTemp.remove(0);
-        }
     }
 
-    private void print(List<Byte> data) {
-        byte[] array = new byte[data.size()];
-        int length = data.size();
+    private void print() {
+        int length = readTemp.size();
+        byte[] array = new byte[length];
         for (int i = 0; i < length; i++) {
-            array[i] = data.get(i);
+            array[i] = readTemp.get(i);
         }
         Log.i(TAG, "--receiveData= " + ByteUtil.bytesToHexString(array));
     }
