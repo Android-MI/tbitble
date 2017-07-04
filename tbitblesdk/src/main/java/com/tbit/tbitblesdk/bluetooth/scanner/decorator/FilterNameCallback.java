db18c9acaa2212506ecc413c95a0af2bc5412444
billy96322 <billy96322@gmail.com>
2017-07-04 15:39:40 +0800

    1. 修复滤设备名字时，设备无设备名的bug

--- tbitblesdk/src/main/java/com/tbit/tbitblesdk/bluetooth/scanner/decorator/FilterNameCallback.java
+++ tbitblesdk/src/main/java/com/tbit/tbitblesdk/bluetooth/scanner/decorator/FilterNameCallback.java
@@ -31,6 +31,7 @@ public class FilterNameCallback extends BaseCallback {
     @Override
     public void onDeviceFounded(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
         String name = bluetoothDevice.getName();
+        if (TextUtils.isEmpty(name)) return;
         if (isTotalMatch) {
             if (TextUtils.equals(deviceName, name)) {
                 callback.onDeviceFounded(bluetoothDevice, i, bytes);
